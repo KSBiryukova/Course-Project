a2db656a31fe6c34d9a2400aabe46a23e2ec2e78
KSBiryukova <K.S.Biryukova@mail.ru>
2016-05-13 21:21:59 +0300

    db3
    db3

--- sql/model.sql
+++ sql/model.sql
@@ -1,23 +1,23 @@
 -- таблица людей, участвующих в обследовании
 CREATE TABLE person(
-	id					int NOT NULL,			  -- id
-	name				varchar(15) NOT NULL,	  -- имя
-	surname				varchar(20) NOT NULL,     -- фамилия
-	company				varchar(50) NULL,         -- место аботы
-	position			varchar(100) NULL,        -- должность (старший, младший)
-	profession  		varchar(50) NULL,         -- профессия
-	
-	PRIMARY KEY (id)
-)
-GO
-
--- таблица пациентов
-CREATE TABLE patient(
-	id					int NOT NULL,             -- id
-	person_id			int NOT NULL,             -- id участника обследования
-	adress				varchar(100) NULL,        -- адрес
-	blood_type  		varchar(2) NULL,          -- группа крови
-	cf_normal   		int NOT NULL,             -- КЧСМ (нормальный)
+	id					int NOT NULL,			    -- id
+	name				varchar(15) NOT NULL,	    -- имя
+	surname				varchar(20) NOT NULL,       -- фамилия
+	company_id			varchar(50) NULL,           -- место аботы
+	position_id			varchar(100) NULL,          -- должность (старший, младший)
+	profession  		varchar(50) NULL,           -- профессия
+	                                                
+	PRIMARY KEY (id)                                
+)                                                   
+GO                                                  
+                                                    
+-- таблица пациентов                                
+CREATE TABLE patient(                               
+	id					int NOT NULL,               -- id
+	person_id			int NOT NULL,               -- id участника обследования
+	adress				varchar(100) NULL,          -- адрес
+	blood_type  		varchar(2) NULL,            -- группа крови
+	cf_normal   		int NOT NULL,               -- КЧСМ (нормальный)
 	
 	PRIMARY KEY (id)
 )
@@ -25,8 +25,8 @@ GO
 
 -- таблица врачей
 CREATE TABLE doctor(
-	id					int NOT NULL,             -- id
-	person_id			int NOT NULL,             -- id врача
+	id					int NOT NULL,               -- id
+	person_id			int NOT NULL,               -- id врача
 	
 	PRIMARY KEY (id)
 )
@@ -34,39 +34,72 @@ GO
 
 -- таблица компаний
 CREATE TABLE company(
-	name				varchar(50) NOT NULL,     -- название компании
-	activity    		varchar(100) NULL,        -- область деятельности
-	risk				varchar(20) NULL,         -- степень важности медицинских показаний
+	id 					int NOT NULL, 				-- id		
+	name				varchar(50) NOT NULL,		-- название компании
+	activity    		varchar(100) NULL,			-- область деятельности
+	risk				varchar(20) NULL,			-- степень важности медицинских показаний
 	
-	PRIMARY KEY (name)
+	PRIMARY KEY (id)
 )
 GO
 
 -- таблица должностей
 CREATE TABLE position(
-	name				varchar(100) NOT NULL,    -- наименование должности
-	lvl					varchar(20) NULL,		  -- должность (старший, младший)
+	id					int NOT NULL,				-- id
+	name				varchar(100) NOT NULL,      -- наименование должности
+	lvl					varchar(20) NULL,		    -- должность (старший, младший)
 		
-	PRIMARY KEY (name)
+	PRIMARY KEY (id)
 )
 GO
 
--- таблица посещений
-CREATE TABLE diagnostic(
-	id					int NOT NULL,             -- id
-	patient_id			int NOT NULL,             -- id пациента
-	doctor_id			int NOT NULL,             -- id врача
-	medicines			varchar(500) NOT NULL,    -- принимаемые лекарства
-	cf_now				int NOT NULL,             -- КЧСМ действующее
-	analysis			varchar(100) NULL,        -- результаты анализов
-	proc_done			varchar(100) NULL,        -- пройденые процедуры
-	diagnosis			varchar(2500) NOT NULL,   -- диагнозы
-	lat_visit			date NOT NULL,            -- последнее посещение
-	suspend_till		date NULL,                -- время больничного (возможная нетрудоспообность)
+-- таблица лекарств
+CREATE TABLE meds(
+	id					int NOT NULL,				-- id
+	med_type			varchar(50) NOT NULL,		-- тип лекарственного средства
+	name				varchar(150) NOT NULL,		-- название препарата
+	
+	PRIMARY KEY(id)
+)
+
+-- таблица принимаемых лекарств
+CREATE TABLE cure(
+	id					int NOT NULL,			    -- id
+	patient_id			int NOT NULL,			    -- id пациента
+	doc_id				int NOT NULL,				-- id врача
+	meds_id				int NOT NULL,			    -- id лекарства
+	date_start			date NOT NULL,			    -- дата начала приема
+	date_end			date NOT NULL,			    -- дата конца приема
+	                                                
+	PRIMARY KEY(id)                                 
+)                                                   
+                                                    
+-- таблица результатов измерения КЧСМ               
+CREATE TABLE measure(                               
+	id 					int NOT NULL,			    -- id
+	patient_id 			int NOT NULL,			    -- id пациента
+	doc_id 				int NOT NULL,			    -- id доктора
+	crit_f 				int NOT NULL,			    -- КЧСМ
+	                                                
+	PRIMARY KEY(id)                                 
+)                                                   
+                                                    
+-- таблица посещений                                
+CREATE TABLE diagnostic(                            
+	id					int NOT NULL,               -- id
+	patient_id			int NOT NULL,               -- id пациента
+	doctor_id			int NOT NULL,               -- id врача
+	analysis			varchar(100) NULL,          -- результаты анализов
+	proc_done			varchar(100) NULL,          -- пройденые процедуры
+	diagnosis			varchar(2500) NOT NULL,     -- диагнозы
+	d_comment			varchar(5000) NULL,			-- комментарии к диагнозу
+	lat_visit			date NOT NULL,              -- последнее посещение
+	suspend_till		date NULL,                  -- время больничного (возможная нетрудоспообность до такого-то числа)
 	
 	PRIMARY KEY (id)
 )
 GO
+
 
 -- внешние ключи --
 
@@ -91,11 +124,36 @@ ALTER TABLE patient
 GO
 
 ALTER TABLE person
-    ADD FOREIGN KEY (company)
+    ADD FOREIGN KEY (company_id)
 		REFERENCES company
 GO
 
 ALTER TABLE person
-    ADD FOREIGN KEY (position)
+    ADD FOREIGN KEY (position_id)
 		REFERENCES position
+GO
+
+ALTER TABLE cure
+    ADD FOREIGN KEY (meds_id)
+		REFERENCES meds
+GO
+
+ALTER TABLE cure
+    ADD FOREIGN KEY (doc_id)
+		REFERENCES doctor
+GO
+
+ALTER TABLE cure
+    ADD FOREIGN KEY (patient_id)
+		REFERENCES patient
+GO
+
+ALTER TABLE measure
+    ADD FOREIGN KEY (patient_id)
+		REFERENCES patient
+GO
+
+ALTER TABLE measure
+    ADD FOREIGN KEY (doc_id)
+		REFERENCES doctor
 GO
